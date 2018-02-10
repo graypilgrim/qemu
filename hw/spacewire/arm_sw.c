7e9aafb45629d8a250f4256735fdf2c816ded1ea
Lukasz Wlazly <lukwlazly@gmail.com>
2018-02-10 11:56:03 +0100

    Add creating RMAP header

--- hw/spacewire/arm_sw.c
+++ hw/spacewire/arm_sw.c
@@ -11,7 +11,14 @@
 #include "hw/sysbus.h"
 #include "qemu/error-report.h"
 
-#define SPW_FIFO_LENGTH 128
+#include <time.h>
+
+#define SPW_FIFO_LENGTH 64
+#define RMAP_CONST_PACKET_LEN 18
+#define RMAP_PROTOCOL_IDENTIFIER 0x01
+#define RMAP_PACKET_TYPE_COMMAND 0x40
+#define RMAP_PACKET_WRITE_COMMAND 0x20
+#define RMAP_EOP 0
 
 #define SPW_TRANSMIT_DESC_HEADER_MASK 0x000000FF
 #define SPW_TRANSMIT_DESC_NONCRC_MASK 0x00000F00
@@ -22,7 +29,7 @@
 #define SPW_TRANSMIT_DESC_APPEND_HEADER_CRC_MASK 0x00010000
 #define SPW_TRANSMIT_DESC_APPEND_DATA_CRC_MASK 0x00020000
 #define SPW_TRANSMIT_DESC_CRC_TYPE_MASK 0x000C0000
-#define SPW_TRANSMIT_DESC_RESERVED 0xFFF00000
+#define SPW_TRANSMIT_DESC_DATA_LEN_MASK 0x00FFFFFF
 
 #define TYPE_SPACEWIRE "spacewire"
 #define SPACEWIRE(obj) OBJECT_CHECK(SpaceWireState, (obj), TYPE_SPACEWIRE)
@@ -49,6 +56,8 @@ typedef struct _SpWRegisters {
     volatile uint32_t dmaAddress;
 } SpWRegisters;
 
+SpWRegisters registers;
+
 typedef struct {
     volatile uint32_t word0;
     volatile uint32_t word1;
@@ -57,6 +66,41 @@ typedef struct {
 } SpWTransmitDescriptor;
 
 static SpWTransmitDescriptor *descriptor_list_head;
+
+unsigned char calculate_crc(unsigned char *data, unsigned int len)
+{
+  return 0;
+}
+
+unsigned char* create_rmap_packet(SpWTransmitDescriptor *descriptor)
+{
+  unsigned data_len = descriptor->word2 & SPW_TRANSMIT_DESC_DATA_LEN_MASK;
+  unsigned char* packet = (unsigned char*)calloc(1, sizeof(RMAP_CONST_PACKET_LEN + data_len));
+
+  // packet[0] Destination logical address
+  packet[1] = RMAP_PROTOCOL_IDENTIFIER;
+  packet[2] |= RMAP_PACKET_TYPE_COMMAND;
+  packet[2] |= RMAP_PACKET_WRITE_COMMAND;
+  packet[3] = registers.destinationKey;
+  // packet[4] Same logical dmaAddress
+
+  srand(time(NULL));
+  int transaction_identifier = rand() & 0x0000FFFF;
+  packet[5] = (transaction_identifier & 0x0000FF00) >> 8;
+  packet[6] = transaction_identifier & 0x000000FF;
+  // packet[7] Extended write dmaAddress
+  // packet[8] .. packet[11] write address
+  packet[12] = (data_len & 0x0000FF0000) >> 16;
+  packet[13] = (data_len & 0x000000FF00) >> 8;
+  packet[14] = data_len & 0x00000000FF;
+  packet[15] = calculate_crc(packet, 10);
+  unsigned char *data_ptr = (unsigned char *)descriptor->word3;
+  memcpy(packet + 16, data_ptr, data_len);
+  packet[16 + data_len] = calculate_crc(data_ptr, data_len);
+  packet[16 + data_len + 1] = RMAP_EOP;
+
+  return packet;
+}
 
 static uint64_t space_wire_read(void *opaque, hwaddr offset,
                            unsigned size)
